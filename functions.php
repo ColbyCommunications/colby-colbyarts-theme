f38d2e59537f04825815183a5481698f1905d208
Gregory Spraggins <gregoryspragginsjr@gmail.com>
2023-06-10 20:08:19 -0400

    Initial commit for Arts. Setup

---
+++ functions.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Timber starter-theme
+ * https://github.com/timber/starter-theme
+ *
+ * @package    WordPress
+ * @subpackage Timber
+ * @since      Timber 0.1
+ */
+
+/**
+ * If you are installing Timber as a Composer dependency in your theme, you'll need this block
+ * to load your dependencies and initialize Timber. If you are using Timber via the WordPress.org
+ * plug-in, you can safely delete this block.
+ */
+$composer_autoload = __DIR__ . '/vendor/autoload.php';
+if ( file_exists( $composer_autoload ) ) {
+	include_once $composer_autoload;
+	$timber = new Timber\Timber();
+}
+
+/**
+ * This ensures that Timber is loaded and available as a PHP class.
+ * If not, it gives an error message to help direct developers on where to activate
+ */
+if ( ! class_exists( 'Timber' ) ) {
+
+	add_action(
+		'admin_notices',
+		function () {
+			echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+		}
+	);
+
+	add_filter(
+		'template_include',
+		function ( $template ) {
+			return get_template_directory() . '/static/no-timber.html';
+		}
+	);
+	return;
+}
+
+/**
+ * Sets the directories (inside your theme) to find .twig files
+ */
+$static_timber_dirs = array(
+	'src/twig/layouts',
+	'src/twig/pages',
+	'src/twig/components',
+);
+
+$theme_base = get_stylesheet_directory() . '/';
+
+$component_dirs = array_map(
+	function ( $str ) use ( &$theme_base ) {
+		return str_replace( $theme_base, '', $str );
+	},
+	array_filter( glob( $theme_base . 'src/twig/components/*' ), 'is_dir' )
+);
+
+Timber::$dirname = array_merge( $static_timber_dirs, $component_dirs );
+
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
+class ArtsSite extends Timber\Site {
+
+	/**
+	 * Add timber support.
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'acf/init', array( $this, 'my_acf_init' ) );
+		parent::__construct();
+	}
+
+	/**
+	 * This is where you can register custom post types.
+	 */
+	public function register_post_types() {
+		// REGISTER POST TYPE HERE
+	}
+
+	public function my_acf_init() {
+
+		// check function exists
+		if ( function_exists( 'acf_register_block' ) ) {
+
+			// REGISTER BLOCKS HERE
+		}
+	}
+}
+
+new ArtsSite();
+
+/**
+ * Enqueue scripts and styles.
+ */
+function arts_theme_scripts() {
+	// remove parent
+	wp_dequeue_style( 'hvh' );
+
+	// child
+	wp_enqueue_style( 'child_css', get_stylesheet_directory_uri() . '/dist/styles/scripts.css', array(), date( 'H:i:s' ) );
+	wp_enqueue_script( 'child_scripts', get_stylesheet_directory_uri() . '/dist/scripts.js', array(), date( 'H:i:s' ), true );
+}
+add_action( 'wp_enqueue_scripts', 'arts_theme_scripts', 100 );
+
+
+
---
+++ src/twig/components/footer/footer.config.js
@@ -0,0 +1,108 @@
+module.exports = {
+  title: "Footer",
+  status: "wip",
+  context: {
+    address: {
+      text: 'Colby College<br />Mayflower Hill Drive<br />Waterville, ME 04901',
+      url: 'https://goo.gl/maps/zcWYwjKYgYRgyvfh6'
+    },
+    phone: '207-859-4000',
+    main: [
+      {
+        title: 'Colby Impact',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Ways to Give',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Alumni',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Volunteer',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Families',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Clubs',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'News',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Alumni Council',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Events',
+        url: 'https://welcometruth.com/',
+      },
+      {
+        title: 'Alumni Awards',
+        url: 'https://welcometruth.com/',
+      },
+    ],
+    utility: [
+      {
+        title: 'Contact',
+        url: 'https://welcometruth.com/',
+        icon: 'email',
+      },
+      {
+        title: 'Give Now',
+        url: 'https://welcometruth.com/',
+        icon: 'heart',
+      },
+      {
+        title: 'Subscribe to Colby News',
+        url: 'https://welcometruth.com/',
+        icon: 'subscribe',
+      },
+    ],
+    social: [
+      {
+        platform: {
+          title: 'Instagram',
+          url: 'https://welcometruth.com/',
+        },
+        icon: 'instagram',
+      },
+      {
+        platform: {
+          title: 'Facebook',
+          url: 'https://welcometruth.com/',
+        },
+        icon: 'facebook',
+      },
+      {
+        platform: {
+          title: 'LinkedIn',
+          url: 'https://welcometruth.com/',
+        },
+        icon: 'linkedin',
+      },
+      {
+        platform: {
+          title: 'Twitter',
+          url: 'https://welcometruth.com/',
+        },
+        icon: 'twitter',
+      },
+      {
+        platform: {
+          title: 'Youtube',
+          url: 'https://welcometruth.com/',
+        },
+        icon: 'youtube',
+      },
+    ],
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dolor elit, nec scelerisque tellus porttitor in. Quisque molestie condimentum lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas dolor elit, nec scelerisque tellus porttitor in. Quisque molestie condimentum lobortis',
+  },
+}
